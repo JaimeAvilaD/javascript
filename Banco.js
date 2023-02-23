420e7d4437a0857fe7c2fa3c21d555e438d01979
Jaime Avila <jaimeaviladominguez1983@gmail.com>
2023-02-22 23:28:08 -0600

    opcion 1 terminada

--- Banco.js
+++ Banco.js
@@ -4,25 +4,34 @@ console.log("***************************");
 
 
 console.log("1. Registrar nuevo usuario");
-console.log("2. Consultar los usuarios registrados");
+console.log("2. Consultar los usuarios registrados\n");
 
 
 //var usuarios = Array();
 
 do{
-    var op = prompt("Seleccione la opción que desea realizar");
+    var op = prompt("\nSeleccione la opción que desea realizar: ");
+    console.log("\n");
+    
     if(op==1){
-        var nombre = prompt("Ingrese su nombre completo");
-        var contraseña = prompt("Ingrese una contraeña");
-        var alcaldia = prompt("Ingrese la alcaldia donde vive");
+        var usuario = [];
+        /*const salary = Number(prompt("ingrese el valor del salario del trabajador"));
+        salaries.push(salary);*/
+        var nombre = prompt("Ingrese su nombre completo: ");
+        var genero = prompt("Ingrese su género: ");
+        var contraseña = prompt("Ingrese una contraeña(4 caracteres): ");
+        var alcaldia = prompt("Ingrese la alcaldia donde vive: ");
+        console.log("Usuario registrado Satisfactoriamente\n");
+        console.log("Su numero de cuenta es:" + alcaldia.slice(0,2)+genero.slice(0,1)+"A"+1);
+        
     }else if(op==2){
-        for(var i=0; i<3; i++);
+        
+            
     }else{
-
-}
-
+        console.log("Sesión Terminada");
+}   
+console.log("Opción invalida. Intente nuevamente.")
 }while(op>0 && op<3);
-
 
 /*
 var info = Array();
